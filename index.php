74b66e9e6dea8c2dfa26d7f4b4e92b9170a45c57
Seb <duinopi3@gmail.com>
2019-05-13 09:31:29 +0200

    1er commit/fichiers vides

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="asset/CSS/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+
+
+	<script src="asset/JS/script.js"></script>
+</body>
+</html>
